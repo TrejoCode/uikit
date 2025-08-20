f3b1259b77d802f4fbfd3781d07b2c7e5605fa2a
Sergio Alejandro Trejo Cuxim <alejandrotrejocode@gmail.com>
2025-08-19 19:18:11 -0500

    fix: change release config to mjs

---
+++ release.config.mjs
@@ -0,0 +1,32 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: [
+    "main",
+    {
+      name: "main",
+      prerelease: true,
+    },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "dist/**"],
+        message:
+          "chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
